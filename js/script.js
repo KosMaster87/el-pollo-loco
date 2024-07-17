90b4640a6c4974e996842a3d1a9dbc701b8531a3
Konstantin Aksenov <kav.pbg@gmail.com>
2024-07-17 18:20:56 -0400

    nun weiter im html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 "use strict";
 
+// ------------------------------------------------------------------------
+// HTML manager
+// ------------------------------------------------------------------------
 function startGame() {
   document.getElementById("startLayerForCanvas").style.display = "none";
   document.getElementById("canvas").style.display = "block";
@@ -26,6 +29,38 @@ function restart() {
   initGame();
 }
 
+// ------------------------------------------------------------------------
+// PopUp Event
+// ------------------------------------------------------------------------
+/**
+ * Open the "instruction PopUp".
+ */
+function openInstructionPopUpSelf() {
+  let popUp = document.getElementById("instructionPopUpSelf");
+
+  if (popUp.style.display == "block") {
+    popUp.style.display = "none";
+    document.removeEventListener("click", closeInstructionPopUpSelf_outside);
+  } else {
+    popUp.style.display = "block";
+    document.addEventListener("click", closeInstructionPopUpSelf_outside);
+  }
+}
+
+/**
+ * Close the "instruction PopUp" with an eventlistener and remove the eventlistener.
+ */
+function closeInstructionPopUpSelf_outside(event) {
+  let popUp = document.getElementById("instructionPopUpSelf");
+  let instructionBtn = document.getElementById("instructionPopUpButton");
+
+  if (!popUp.contains(event.target) && event.target !== instructionBtn) {
+    popUp.style.display = "none";
+    document.removeEventListener("click", closeInstructionPopUpSelf_outside);
+  }
+}
+
+// ------------------------------------------------------------------------
 function clearAllIntervals() {
   for (let i = 1; i < 9999; i++) window.clearInterval(i);
 }
